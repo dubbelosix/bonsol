5ec0e720756be573cb040d39acd1348353c63a6a
dubbelosix <dub@006.com>
2024-10-20 10:57:40 +0530

    wip

--- validator.sh
+++ validator.sh
@@ -8,4 +8,4 @@ cargo build-sbf && solana-test-validator \
    --rpc-pubsub-enable-block-subscription \
     --bpf-program BoNsHRcyLLNdtnoDf8hiCNZpyehMC4FDMxs6NTxFi3ew target/deploy/bonsol_channel.so  \
     --bpf-program exay1T7QqsJPNcwzMiWubR6vZnqrgM16jZRraHgqBGG target/deploy/callback_example.so \
-    --bpf-program CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d ../mainnet_bpf_programs/core.so -r
+    --bpf-program CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d ../../mainnet_bpf_programs/core.so -r
